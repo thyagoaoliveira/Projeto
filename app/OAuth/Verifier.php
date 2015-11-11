16b99a7420db2f34c114783529a76cb59318cc93
Thyago A Oliveira <thyagoaoliveira@gmail.com>
2015-11-11 20:48:55 -0200

    Instalando e Configurando OAuth 2

---
+++ app/OAuth/Verifier.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Projeto\OAuth;
+
+use Auth;
+
+class Verifier
+{
+	public function verify($username, $password)
+	{
+		$credentials = [
+			'email'    => $username,
+			'password' => $password,
+		];
+
+		if (Auth::once($credentials)) {
+			return Auth::user()->id;
+		}
+
+		return false;
+	}
+}
