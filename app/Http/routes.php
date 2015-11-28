b97c117c194cbe118c30838d9b36979a3db4e71f
Thyago A Oliveira <thyagoaoliveira@gmail.com>
2015-11-28 18:20:00 -0200

    Agrupando Rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,24 +4,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('client', ['middleware'=>'oauth', 'uses'=>'ClientController@index']);
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::put('client/{id}', 'ClientController@update');
-Route::delete('client/{id}', 'ClientController@destroy');
-
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
-
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::put('project/{id}', 'ProjectController@update');
-Route::delete('project/{id}', 'ProjectController@destroy');
-
 Route::post('oauth/access_token', function() {
 	return Response::json(Authorizer::issueAccessToken());
+});
+
+Route::group(['middleware' => 'oauth'], function() {
+	Route::resource('client', 'ClientController', ['except' => ['create', 'edit']]);
+
+	Route::group(['prefix' => 'project'], function() {
+		Route::resource('', 'ProjectController', ['except' => ['create', 'edit']]);
+
+		/*Route::get('{id}/note', 'ProjectNoteController@index');
+		Route::post('{id}/note', 'ProjectNoteController@store');
+		Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
+		Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
+		Route::delete('note/{noteId}', 'ProjectNoteController@destroy');*/
+	});
 });
