0f49b6a5a7de783c9c6cf55e82b1de1b08bea4e7
Thyago A Oliveira <thyagoaoliveira@gmail.com>
2016-05-16 17:22:05 -0300

    Projeto fase II - login

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', ['ngRoute', 'angular-oauth2', 'app.controllers']);
 
-angular.module('app.controllers', ['angular-oauth2']);
+angular.module('app.controllers', ['ngMessages', 'angular-oauth2']);
 
 app.config(['$routeProvider', 'OAuthProvider', function($routeProvider, OAuthProvider) {
 	$routeProvider
@@ -18,7 +18,7 @@ app.config(['$routeProvider', 'OAuthProvider', function($routeProvider, OAuthPro
       baseUrl: 'http://localhost:8000',
       clientId: 'appid1',
       clientSecret: 'secret',
-      granthPath: "oauth/access_token"
+      grantPath: "oauth/access_token"
     });
 }]);
 
--- resources/assets/js/controllers/login.js
+++ resources/assets/js/controllers/login.js
@@ -5,11 +5,19 @@ angular.module('app.controllers')
 		password: ''
 	};
 
+	$scope.error = {
+		message: '',
+		error: false
+	};
+
 	$scope.login = function() {
-		OAuth.getAccessToken($scope.user).then(function() {
-			$location.path('home');
-		}, function() {
-			alert('Login inválido!');
-		});
+		if($scope.form.$valid) {
+			OAuth.getAccessToken($scope.user).then(function() {
+				$location.path('home');
+			}, function(data) {
+				$scope.error.error = true;
+				$scope.error.message = data.data.error_description;
+			});
+		}
 	};
 }]);
